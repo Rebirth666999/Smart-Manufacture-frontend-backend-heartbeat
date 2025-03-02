525e5777ca24bf05c06c646003c497e9041b5d53
YangZY <2021212038@stu.hit.edu.cn>
2025-03-02 11:35:15 +0800

    取消新增设备事件操作

--- frontend/src/views/system/equipmentRecord/index.vue
+++ frontend/src/views/system/equipmentRecord/index.vue
@@ -60,7 +60,7 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
+      <!-- <el-col :span="1.5">
         <el-button
           type="primary"
           plain
@@ -69,7 +69,7 @@
           @click="handleAdd"
           v-hasPermi="['system:equipmentRecord:add']"
         >新增</el-button>
-      </el-col>
+      </el-col> -->
       <el-col :span="1.5">
         <el-button
           type="success"

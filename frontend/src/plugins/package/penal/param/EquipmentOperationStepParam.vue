766f270b56be8b9b2dac8acedd9562629f653095
YangZY <2021212038@stu.hit.edu.cn>
2025-03-03 15:29:29 +0800

    追加参数类型、位置、默认值字段

--- frontend/src/plugins/package/penal/param/EquipmentOperationStepParam.vue
+++ frontend/src/plugins/package/penal/param/EquipmentOperationStepParam.vue
@@ -3,33 +3,65 @@
     <!-- 显示参数表格 -->
     <el-table :data="paramList" size="mini" max-height="240" border fit>
       <el-table-column label="序号" width="50px" type="index" />
-      <el-table-column label="参数名称" prop="name" min-width="100px" show-overflow-tooltip />
-      <el-table-column label="父级参数序号" prop="parent" min-width="100px" show-overflow-tooltip />
+      <el-table-column label="名称" prop="eospa_name" show-overflow-tooltip />
+      <el-table-column label="类型" prop="eospa_type" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.ices_param_type" :value="scope.row.eospa_type"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="位置" prop="eospa_pos" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.ices_param_position" :value="scope.row.eospa_pos"/>
+        </template>
+      </el-table-column>
       <el-table-column label="操作" width="90px">
         <template slot-scope="{ row, $index }">
-          <el-button size="mini" type="text" @click="openAttributesForm(row, $index)">编辑</el-button>
+          <el-button size="mini" type="text" @click="openParamsForm(row, $index)">编辑</el-button>
           <el-divider direction="vertical" />
-          <el-button size="mini" type="text" style="color: #ff4d4f" @click="removeAttributes(row, $index)">移除</el-button>
+          <el-button size="mini" type="text" style="color: #ff4d4f" @click="removeParam(row, $index)">移除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <div class="element-drawer__button">
-      <el-button size="mini" type="primary" icon="el-icon-plus" @click="openAttributesForm(null, -1)">添加参数</el-button>
+      <el-button size="mini" type="primary" icon="el-icon-plus" @click="openParamsForm(null, -1)">添加参数</el-button>
     </div>
 
     <!-- 添加/修改参数窗口 -->
     <el-dialog :visible.sync="propertyFormModelVisible" title="参数配置" width="600px" append-to-body destroy-on-close>
       <el-form :model="propertyForm" label-width="130px" size="mini" ref="attributeFormRef" @submit.native.prevent :rules="rules">
-        <el-form-item label="参数名称：" prop="name">
-          <el-input v-model="propertyForm.name" clearable />
+        <el-form-item label="参数名称：" prop="eospa_name">
+          <el-input v-model="propertyForm.eospa_name" clearable />
         </el-form-item>
         <el-form-item label="父级参数序号：" prop="parent">
           <el-input v-model="propertyForm.parent" clearable />
         </el-form-item>
+        <el-form-item label="参数类型：" prop="eospa_type">
+          <el-select v-model="propertyForm.eospa_type" placeholder="请选择参数类型" style="width: 100%;">
+            <el-option
+              v-for="dict in dict.type.ices_param_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="参数位置：" prop="eospa_pos">
+          <el-select v-model="propertyForm.eospa_pos" placeholder="请选择参数位置" style="width: 100%;">
+            <el-option
+              v-for="dict in dict.type.ices_param_position"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="默认值：" prop="eospa_value">
+          <el-input v-model="propertyForm.eospa_value" clearable />
+        </el-form-item>
       </el-form>
       <template slot="footer">
         <el-button size="mini" @click="propertyFormModelVisible = false">取 消</el-button>
-        <el-button size="mini" type="primary" @click="saveAttribute">确 定</el-button>
+        <el-button size="mini" type="primary" @click="saveParam">确 定</el-button>
       </template>
     </el-dialog>
   </div>
@@ -38,6 +70,7 @@
 <script>
 export default {
   name: "EquipmentOperationStepParam",
+  dicts: ['ices_param_type', 'ices_param_position'],
   props: {
     id: String,
     type: String
@@ -58,8 +91,14 @@ export default {
       propertyFormModelVisible: false,
       // 表单校验
       rules: {
-        name: [
+        eospa_name: [
           { required: true, message: "参数名称不能为空", trigger: "blur" }
+        ],
+        eospa_type: [
+          { required: true, message: "参数类型不能为空", trigger: "blur" }
+        ],
+        eospa_pos: [
+          { required: true, message: "参数位置不能为空", trigger: "blur" }
         ],
       },
     };
@@ -78,7 +117,7 @@ export default {
       this.paramList = JSON.parse(window.bpmnInstances.bpmnElement.businessObject.$attrs.params || '[]')
     },
     // 打开新增/修改参数窗口
-    openAttributesForm(attr, index) {
+    openParamsForm(attr, index) {
       this.editingPropertyIndex = index;
       // 读取要编辑的参数
       this.propertyForm = index === -1 ? {} : JSON.parse(JSON.stringify(attr));
@@ -88,7 +127,7 @@ export default {
       });
     },
     // 删除参数
-    removeAttributes(attr, index) {
+    removeParam(attr, index) {
       this.$confirm("确认移除该参数吗？", "提示", {
         confirmButtonText: "确 认",
         cancelButtonText: "取 消"
@@ -99,20 +138,26 @@ export default {
         .catch(() => console.info("操作取消"));
     },
     // 保存单个参数
-    saveAttribute() {
+    saveParam() {
       this.$refs["attributeFormRef"].validate(valid => {
         if (valid) {
-          const { name, parent } = this.propertyForm;
+          const { eospa_name, parent, eospa_type, eospa_pos, eospa_value } = this.propertyForm
           // 更新显示数组
           if (this.editingPropertyIndex !== -1) {
             // 修改参数
-            this.paramList[this.editingPropertyIndex].name = name
+            this.paramList[this.editingPropertyIndex].eospa_name = eospa_name
             this.paramList[this.editingPropertyIndex].parent = parseInt(parent) || 0
+            this.paramList[this.editingPropertyIndex].eospa_type = eospa_type
+            this.paramList[this.editingPropertyIndex].eospa_pos = eospa_pos
+            this.paramList[this.editingPropertyIndex].eospa_value = eospa_value
           } else {
             // 新建参数
             this.paramList.push({
-              name: name,
-              parent: parseInt(parent) || 0
+              eospa_name: eospa_name,
+              parent: parseInt(parent) || 0,
+              eospa_type: eospa_type,
+              eospa_pos: eospa_pos,
+              eospa_value: eospa_value
             })
           }
           this.updateParams()

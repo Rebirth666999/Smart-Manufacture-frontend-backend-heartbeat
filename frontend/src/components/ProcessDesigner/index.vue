d53704a1852e2467e127308125920693cb2affb6
YangZY <2021212038@stu.hit.edu.cn>
2025-03-13 14:18:05 +0800

    设计器元素悬浮操作菜单

--- frontend/src/components/ProcessDesigner/index.vue
+++ frontend/src/components/ProcessDesigner/index.vue
@@ -25,7 +25,8 @@ import Vue from 'vue';
 import '@/plugins/package/theme/index.scss';
 import { BpmnProcessDesigner, BmpnProcessPenal } from '@/plugins/package/index';
 // 自定义元素选中时的弹出菜单（修改 默认任务 为 用户任务）
-import CustomContentPadProvider from '@/plugins/package/designer/plugins/content-pad';
+import RuoyiContentPadProvider from '@/plugins/package/designer/plugins/content-pad/ruoyi';
+import IndustryContentPadProvider from '@/plugins/package/designer/plugins/content-pad/industry';
 // 自定义左侧菜单（修改 默认任务 为 用户任务）
 import RuoyiPaletteProvider from '@/plugins/package/designer/plugins/palette/ruoyi';
 import EquipmentOperationPaletteProvider from '@/plugins/package/designer/plugins/palette/equipmentOperation'
@@ -75,16 +76,19 @@ export default {
         labelVisible: false,
         prefix: 'flowable',
         headerButtonSize: 'small',
-        additionalModel: [CustomContentPadProvider]
+        additionalModel: []
       }
     }
   },
   created() {
     if (this.mode === 0) {
+      this.controlForm.additionalModel.push(RuoyiContentPadProvider)
       this.controlForm.additionalModel.push(RuoyiPaletteProvider)
     } else if (this.mode === 1) {
+      this.controlForm.additionalModel.push(IndustryContentPadProvider)
       this.controlForm.additionalModel.push(EquipmentOperationPaletteProvider)
     } else {
+      this.controlForm.additionalModel.push(IndustryContentPadProvider)
       this.controlForm.additionalModel.push(ProcessPaletteProvider)
     }
   },

328e319ea8b0c22e9ba65f1d3e415faad946e2a6
YangZY <2021212038@stu.hit.edu.cn>
2025-03-13 13:20:06 +0800

    画板的工艺步骤元素

--- frontend/src/components/ProcessDesigner/index.vue
+++ frontend/src/components/ProcessDesigner/index.vue
@@ -29,6 +29,7 @@ import CustomContentPadProvider from '@/plugins/package/designer/plugins/content
 // 自定义左侧菜单（修改 默认任务 为 用户任务）
 import RuoyiPaletteProvider from '@/plugins/package/designer/plugins/palette/ruoyi';
 import EquipmentOperationPaletteProvider from '@/plugins/package/designer/plugins/palette/equipmentOperation'
+import ProcessPaletteProvider from '@/plugins/package/designer/plugins/palette/process'
 import { vuePlugin } from '@/plugins/package/highlight';
 import 'highlight.js/styles/atom-one-dark-reasonable.css';
 Vue.use(vuePlugin);
@@ -74,8 +75,17 @@ export default {
         labelVisible: false,
         prefix: 'flowable',
         headerButtonSize: 'small',
-        additionalModel: [CustomContentPadProvider, this.mode === 0? RuoyiPaletteProvider:EquipmentOperationPaletteProvider]
+        additionalModel: [CustomContentPadProvider]
       }
+    }
+  },
+  created() {
+    if (this.mode === 0) {
+      this.controlForm.additionalModel.push(RuoyiPaletteProvider)
+    } else if (this.mode === 1) {
+      this.controlForm.additionalModel.push(EquipmentOperationPaletteProvider)
+    } else {
+      this.controlForm.additionalModel.push(ProcessPaletteProvider)
     }
   },
   methods: {

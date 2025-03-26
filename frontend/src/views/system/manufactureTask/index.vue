d186f53c8613bd1411a5d8dd8fa46f64f3ebd9c0
YangZY <2021212038@stu.hit.edu.cn>
2025-03-26 20:33:11 +0800

    追加仓库字段

--- frontend/src/views/system/manufactureTask/index.vue
+++ frontend/src/views/system/manufactureTask/index.vue
@@ -119,22 +119,32 @@
           {{ areaList.find(ele => ele.arCode === scope.row.arCode).arName || '' }}
         </template>
       </el-table-column>
+      <el-table-column label="原料仓库" align="center" prop="stCodeMa">
+        <template slot-scope="scope">
+          {{ materialStoreList.find(ele => ele.stCode === scope.row.stCodeMa).stName || '' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="产品仓库" align="center" prop="stCodePr">
+        <template slot-scope="scope">
+          {{ productStoreList.find(ele => ele.stCode === scope.row.stCodePr).stName || '' }}
+        </template>
+      </el-table-column>
       <el-table-column label="状态" align="center" prop="mtStat">
         <template slot-scope="scope">
           <dict-tag :options="dict.type.ices_order_status" :value="scope.row.mtStat"/>
         </template>
       </el-table-column>
-      <el-table-column label="实际开始时间" align="center" prop="mtBegin" width="180">
+      <el-table-column label="实际开始时间" align="center" prop="mtBegin" width="100">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.mtBegin, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="最晚结束时间" align="center" prop="mtEndPlan" width="180">
+      <el-table-column label="最晚结束时间" align="center" prop="mtEndPlan" width="100">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.mtEndPlan, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="实际结束时间" align="center" prop="mtEndReal" width="180">
+      <el-table-column label="实际结束时间" align="center" prop="mtEndReal" width="100">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.mtEndReal, '{y}-{m}-{d}') }}</span>
         </template>
@@ -210,6 +220,34 @@
             </el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="原料仓库" prop="stCodeMa">
+          <el-select
+            v-model="form.stCodeMa"
+            placeholder="请选择原料仓库"
+          >
+            <el-option
+              v-for="item in materialStoreList"
+              :key="item.stCode"
+              :label="item.stName"
+              :value="item.stCode"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="产品仓库" prop="stCodePr">
+          <el-select
+            v-model="form.stCodePr"
+            placeholder="请选择产品仓库"
+          >
+            <el-option
+              v-for="item in productStoreList"
+              :key="item.stCode"
+              :label="item.stName"
+              :value="item.stCode"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item label="最晚结束时间" prop="mtEndPlan">
           <el-date-picker clearable
             v-model="form.mtEndPlan"
@@ -254,6 +292,7 @@ import { listManufactureTask, getManufactureTask, delManufactureTask, addManufac
 import { listArea } from "@/api/system/area";
 import { listManufacturePlan } from "@/api/system/manufacturePlan";
 import { listProcess, getBpmnXml } from "@/api/system/process";
+import { listStore } from "@/api/system/store";
 
 import { listEquipment } from "@/api/system/equipment";
 import { listEquipmentOperation } from "@/api/system/equipmentOperation";
@@ -315,6 +354,12 @@ export default {
         arCode: [
           { required: true, message: "目标车间不能为空", trigger: "blur" }
         ],
+        stCodeMa: [
+          { required: true, message: "原料仓库不能为空", trigger: "blur" }
+        ],
+        stCodePr: [
+          { required: true, message: "产品仓库不能为空", trigger: "blur" }
+        ],
         mtEndPlan: [
           { required: true, message: "最晚结束时间不能为空", trigger: "blur" }
         ],
@@ -331,6 +376,10 @@ export default {
       manufacturePlanList: [],
       // 工艺流程列表
       processList: [],
+      // 原料仓库列表
+      materialStoreList: [],
+      // 产品仓库列表
+      productStoreList: [],
       // 查看窗口是否打开
       viewerOpen: false,
       // 查看器相关数据
@@ -356,6 +405,7 @@ export default {
     await this.getAreaList();
     await this.getManufacturePlanList();
     await this.getProcessList();
+    await this.getStoreList();
     await this.getReferenceList();
     this.getList();
   },
@@ -380,6 +430,15 @@ export default {
       });
       listEquipmentOperationStepParam().then(response => {
         this.viewerData.eospaList = response.rows;
+      });
+    },
+    // 获取仓库列表
+    getStoreList() {
+      listStore({ stType: '1' }).then(response => {
+        this.materialStoreList = response.rows;
+      });
+      listStore({ stType: '2' }).then(response => {
+        this.productStoreList = response.rows;
       });
     },
     // 获取工艺流程列表
@@ -420,6 +479,8 @@ export default {
         mtId: undefined,
         mpCode: undefined,
         arCode: undefined,
+        stCodeMa: undefined,
+        stCodePr: undefined,
         mtStat: undefined,
         mtBegin: undefined,
         mtEndPlan: undefined,

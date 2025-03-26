170a7d48a8e900497d4054faf78964701791285c
YangZY <2021212038@stu.hit.edu.cn>
2025-03-26 19:45:06 +0800

    代码生成-仓库管理

---
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/domain/IcesStore.java
@@ -0,0 +1,59 @@
+package com.ruoyi.system.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import java.io.Serializable;
+import java.util.Date;
+import java.math.BigDecimal;
+
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 仓库对象 ices_store
+ *
+ * @author ruoyi
+ * @date 2025-03-26
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("ices_store")
+public class IcesStore extends BaseEntity {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     * 仓库ID
+     */
+    @TableId(value = "st_id")
+    private Long stId;
+    /**
+     * 仓库编码
+     */
+    private String stCode;
+    /**
+     * 仓库名称
+     */
+    private String stName;
+    /**
+     * 仓库类型
+     */
+    private String stType;
+    /**
+     * 货位数量
+     */
+    private Long stSpace;
+    /**
+     * 空闲货位数量
+     */
+    private Long stFree;
+    /**
+     * 已删除
+     */
+    private Long stDelete;
+    /**
+     * 描述
+     */
+    private String stDesc;
+
+}
---
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/domain/bo/IcesStoreBo.java
@@ -0,0 +1,70 @@
+package com.ruoyi.system.domain.bo;
+
+import com.ruoyi.common.core.validate.AddGroup;
+import com.ruoyi.common.core.validate.EditGroup;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.validation.constraints.*;
+
+import java.util.Date;
+
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 仓库业务对象 ices_store
+ *
+ * @author ruoyi
+ * @date 2025-03-26
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class IcesStoreBo extends BaseEntity {
+
+    /**
+     * 仓库ID
+     */
+    @NotNull(message = "仓库ID不能为空", groups = { EditGroup.class })
+    private Long stId;
+
+    /**
+     * 仓库编码
+     */
+    private String stCode;
+
+    /**
+     * 仓库名称
+     */
+    @NotBlank(message = "仓库名称不能为空", groups = { AddGroup.class, EditGroup.class })
+    private String stName;
+
+    /**
+     * 仓库类型
+     */
+    @NotBlank(message = "仓库类型不能为空", groups = { AddGroup.class, EditGroup.class })
+    private String stType;
+
+    /**
+     * 货位数量
+     */
+    @NotNull(message = "货位数量不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long stSpace;
+
+    /**
+     * 空闲货位数量
+     */
+    @NotNull(message = "空闲货位数量不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long stFree;
+
+    /**
+     * 已删除
+     */
+    private Long stDelete;
+
+    /**
+     * 描述
+     */
+    private String stDesc;
+
+
+}

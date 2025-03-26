02a7016235294dd9a4a9ab9df463208e2e1663e2
YangZY <2021212038@stu.hit.edu.cn>
2025-03-26 19:02:34 +0800

    引入编码

--- backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesMaterialServiceImpl.java
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesMaterialServiceImpl.java
@@ -7,6 +7,7 @@ import com.ruoyi.common.core.domain.PageQuery;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ruoyi.system.service.IIcesCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.domain.bo.IcesMaterialBo;
@@ -30,6 +31,7 @@ import java.util.Collection;
 public class IcesMaterialServiceImpl implements IIcesMaterialService {
 
     private final IcesMaterialMapper baseMapper;
+    private final IIcesCodeService codeService;
 
     /**
      * 查询物料
@@ -72,6 +74,7 @@ public class IcesMaterialServiceImpl implements IIcesMaterialService {
      */
     @Override
     public Boolean insertByBo(IcesMaterialBo bo) {
+        bo.setMaCode(codeService.insertByType("Material"));
         IcesMaterial add = BeanUtil.toBean(bo, IcesMaterial.class);
         validEntityBeforeSave(add);
         boolean flag = baseMapper.insert(add) > 0;

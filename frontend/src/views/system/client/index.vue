a2e2a747cfeabcf9dfa9be01c19b661a667e3afc
YangZY <2021212038@stu.hit.edu.cn>
2025-03-28 10:04:14 +0800

    样式优化

--- frontend/src/views/system/client/index.vue
+++ frontend/src/views/system/client/index.vue
@@ -159,86 +159,130 @@
     />
 
     <!-- 添加或修改客户基本信息对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="客户等级" prop="cllCode">
-          <el-input v-model="form.cllCode" placeholder="请输入客户等级" />
-        </el-form-item>
-        <el-form-item label="业务员ID" prop="clOperator">
-          <el-input v-model="form.clOperator" placeholder="请输入业务员ID" />
-        </el-form-item>
-        <el-form-item label="客户名称(中)" prop="clName">
-          <el-input v-model="form.clName" placeholder="请输入客户名称(中)" />
-        </el-form-item>
-        <el-form-item label="客户名称(英)" prop="clNameEn">
-          <el-input v-model="form.clNameEn" placeholder="请输入客户名称(英)" />
-        </el-form-item>
-        <el-form-item label="法人代表" prop="clLegalRepres">
-          <el-input v-model="form.clLegalRepres" placeholder="请输入法人代表" />
-        </el-form-item>
-        <el-form-item label="企业联系人" prop="clContact">
-          <el-input v-model="form.clContact" placeholder="请输入企业联系人" />
-        </el-form-item>
-        <el-form-item label="联系电话1" prop="clPhone1">
-          <el-input v-model="form.clPhone1" placeholder="请输入联系电话1" />
-        </el-form-item>
-        <el-form-item label="联系电话2" prop="clPhone2">
-          <el-input v-model="form.clPhone2" placeholder="请输入联系电话2" />
-        </el-form-item>
-        <el-form-item label="传真" prop="clFax">
-          <el-input v-model="form.clFax" placeholder="请输入传真" />
-        </el-form-item>
-        <el-form-item label="邮政编码" prop="clPostcode">
-          <el-input v-model="form.clPostcode" placeholder="请输入邮政编码" />
-        </el-form-item>
-        <el-form-item label="联系地址" prop="clAddr">
-          <el-input v-model="form.clAddr" placeholder="请输入联系地址" />
-        </el-form-item>
-        <el-form-item label="企业性质" prop="clType">
-          <el-input v-model="form.clType" placeholder="请输入企业性质" />
-        </el-form-item>
-        <el-form-item label="信用等级" prop="clCreditRank">
-          <el-input v-model="form.clCreditRank" placeholder="请输入信用等级" />
-        </el-form-item>
-        <el-form-item label="信用额度" prop="clCreditQuota">
-          <el-input v-model="form.clCreditQuota" placeholder="请输入信用额度" />
-        </el-form-item>
-        <el-form-item label="注册资金" prop="clRegisterCaptial">
-          <el-input v-model="form.clRegisterCaptial" placeholder="请输入注册资金" />
-        </el-form-item>
-        <el-form-item label="注册号码" prop="clRegisterNo">
-          <el-input v-model="form.clRegisterNo" placeholder="请输入注册号码" />
-        </el-form-item>
-        <el-form-item label="银行卡号" prop="clBankNum">
-          <el-input v-model="form.clBankNum" placeholder="请输入银行卡号" />
-        </el-form-item>
-        <el-form-item label="开户行" prop="clBank">
-          <el-input v-model="form.clBank" placeholder="请输入开户行" />
-        </el-form-item>
-        <el-form-item label="网址" prop="clWebsite">
-          <el-input v-model="form.clWebsite" placeholder="请输入网址" />
-        </el-form-item>
-        <el-form-item label="注册日期" prop="clRegisterDate">
-          <el-date-picker clearable
-            v-model="form.clRegisterDate"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm:ss"
-            placeholder="请选择注册日期">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="状态" prop="clStat">
-          <el-select v-model="form.clStat" placeholder="请选择状态">
-            <el-option
-              v-for="dict in dict.type.ices_client_status"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="描述" prop="clDesc">
-          <el-input v-model="form.clDesc" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
+    <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="110px">
+        <el-col :span="12">
+          <el-form-item label="客户等级" prop="cllCode">
+            <el-input v-model="form.cllCode" placeholder="请输入客户等级" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="业务员ID" prop="clOperator">
+            <el-input v-model="form.clOperator" placeholder="请输入业务员ID" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="客户名称(中)" prop="clName">
+            <el-input v-model="form.clName" placeholder="请输入客户名称(中)" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="客户名称(英)" prop="clNameEn">
+            <el-input v-model="form.clNameEn" placeholder="请输入客户名称(英)" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="法人代表" prop="clLegalRepres">
+            <el-input v-model="form.clLegalRepres" placeholder="请输入法人代表" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="企业联系人" prop="clContact">
+            <el-input v-model="form.clContact" placeholder="请输入企业联系人" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="联系电话1" prop="clPhone1">
+            <el-input v-model="form.clPhone1" placeholder="请输入联系电话1" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="联系电话2" prop="clPhone2">
+            <el-input v-model="form.clPhone2" placeholder="请输入联系电话2" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="传真" prop="clFax">
+            <el-input v-model="form.clFax" placeholder="请输入传真" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="邮政编码" prop="clPostcode">
+            <el-input v-model="form.clPostcode" placeholder="请输入邮政编码" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="联系地址" prop="clAddr">
+            <el-input v-model="form.clAddr" placeholder="请输入联系地址" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="企业性质" prop="clType">
+            <el-input v-model="form.clType" placeholder="请输入企业性质" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="信用等级" prop="clCreditRank">
+            <el-input v-model="form.clCreditRank" placeholder="请输入信用等级" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="信用额度" prop="clCreditQuota">
+            <el-input v-model="form.clCreditQuota" placeholder="请输入信用额度" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="注册资金" prop="clRegisterCaptial">
+            <el-input v-model="form.clRegisterCaptial" placeholder="请输入注册资金" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="注册号码" prop="clRegisterNo">
+            <el-input v-model="form.clRegisterNo" placeholder="请输入注册号码" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="银行卡号" prop="clBankNum">
+            <el-input v-model="form.clBankNum" placeholder="请输入银行卡号" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="开户行" prop="clBank">
+            <el-input v-model="form.clBank" placeholder="请输入开户行" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="网址" prop="clWebsite">
+            <el-input v-model="form.clWebsite" placeholder="请输入网址" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="注册日期" prop="clRegisterDate">
+            <el-date-picker clearable
+              v-model="form.clRegisterDate"
+              type="datetime"
+              value-format="yyyy-MM-dd HH:mm:ss"
+              placeholder="请选择注册日期">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="状态" prop="clStat">
+            <el-select v-model="form.clStat" placeholder="请选择状态">
+              <el-option
+                v-for="dict in dict.type.ices_client_status"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="描述" prop="clDesc">
+            <el-input v-model="form.clDesc" type="textarea" placeholder="请输入内容" />
+          </el-form-item>
+        </el-col>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
@@ -441,3 +485,11 @@ export default {
   }
 };
 </script>
+<style scoped>
+.el-select {
+  width: 100%;
+}
+.el-date-editor{
+  width: 100%;
+}
+</style>

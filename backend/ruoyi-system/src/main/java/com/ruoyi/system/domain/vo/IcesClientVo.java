b6faa6c43bb2c09ff9dcfae66b7c0d8f755e082e
YangZY <2021212038@stu.hit.edu.cn>
2025-03-28 09:51:40 +0800

    代码生成-客户基本信息

---
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/domain/vo/IcesClientVo.java
@@ -0,0 +1,178 @@
+package com.ruoyi.system.domain.vo;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.ruoyi.common.annotation.ExcelDictFormat;
+import com.ruoyi.common.convert.ExcelDictConvert;
+import lombok.Data;
+import java.util.Date;
+
+
+
+/**
+ * 客户基本信息视图对象 ices_client
+ *
+ * @author ruoyi
+ * @date 2025-03-28
+ */
+@Data
+@ExcelIgnoreUnannotated
+public class IcesClientVo {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 基本信息ID
+     */
+    @ExcelProperty(value = "基本信息ID")
+    private Long clId;
+
+    /**
+     * 客户编码
+     */
+    @ExcelProperty(value = "客户编码")
+    private String clCode;
+
+    /**
+     * 客户等级
+     */
+    @ExcelProperty(value = "客户等级")
+    private String cllCode;
+
+    /**
+     * 业务员ID
+     */
+    @ExcelProperty(value = "业务员ID")
+    private Long clOperator;
+
+    /**
+     * 客户名称(中)
+     */
+    @ExcelProperty(value = "客户名称(中)")
+    private String clName;
+
+    /**
+     * 客户名称(英)
+     */
+    @ExcelProperty(value = "客户名称(英)")
+    private String clNameEn;
+
+    /**
+     * 法人代表
+     */
+    @ExcelProperty(value = "法人代表")
+    private String clLegalRepres;
+
+    /**
+     * 企业联系人
+     */
+    @ExcelProperty(value = "企业联系人")
+    private String clContact;
+
+    /**
+     * 联系电话1
+     */
+    @ExcelProperty(value = "联系电话1")
+    private String clPhone1;
+
+    /**
+     * 联系电话2
+     */
+    @ExcelProperty(value = "联系电话2")
+    private String clPhone2;
+
+    /**
+     * 传真
+     */
+    @ExcelProperty(value = "传真")
+    private String clFax;
+
+    /**
+     * 邮政编码
+     */
+    @ExcelProperty(value = "邮政编码")
+    private String clPostcode;
+
+    /**
+     * 联系地址
+     */
+    @ExcelProperty(value = "联系地址")
+    private String clAddr;
+
+    /**
+     * 企业性质
+     */
+    @ExcelProperty(value = "企业性质")
+    private String clType;
+
+    /**
+     * 信用等级
+     */
+    @ExcelProperty(value = "信用等级")
+    private String clCreditRank;
+
+    /**
+     * 信用额度
+     */
+    @ExcelProperty(value = "信用额度")
+    private String clCreditQuota;
+
+    /**
+     * 注册资金
+     */
+    @ExcelProperty(value = "注册资金")
+    private String clRegisterCaptial;
+
+    /**
+     * 注册号码
+     */
+    @ExcelProperty(value = "注册号码")
+    private String clRegisterNo;
+
+    /**
+     * 银行卡号
+     */
+    @ExcelProperty(value = "银行卡号")
+    private String clBankNum;
+
+    /**
+     * 开户行
+     */
+    @ExcelProperty(value = "开户行")
+    private String clBank;
+
+    /**
+     * 网址
+     */
+    @ExcelProperty(value = "网址")
+    private String clWebsite;
+
+    /**
+     * 注册日期
+     */
+    @ExcelProperty(value = "注册日期")
+    private Date clRegisterDate;
+
+    /**
+     * 状态
+     */
+    @ExcelProperty(value = "状态", converter = ExcelDictConvert.class)
+    @ExcelDictFormat(dictType = "ices_client_status")
+    private String clStat;
+
+    /**
+     * 已删除
+     */
+    @ExcelProperty(value = "已删除")
+    private Long clDelete;
+
+    /**
+     * 描述
+     */
+    @ExcelProperty(value = "描述")
+    private String clDesc;
+
+
+}
---
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesClientServiceImpl.java
@@ -0,0 +1,113 @@
+package com.ruoyi.system.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.core.domain.PageQuery;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.ruoyi.system.domain.bo.IcesClientBo;
+import com.ruoyi.system.domain.vo.IcesClientVo;
+import com.ruoyi.system.domain.IcesClient;
+import com.ruoyi.system.mapper.IcesClientMapper;
+import com.ruoyi.system.service.IIcesClientService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Collection;
+
+/**
+ * 客户基本信息Service业务层处理
+ *
+ * @author ruoyi
+ * @date 2025-03-28
+ */
+@RequiredArgsConstructor
+@Service
+public class IcesClientServiceImpl implements IIcesClientService {
+
+    private final IcesClientMapper baseMapper;
+
+    /**
+     * 查询客户基本信息
+     */
+    @Override
+    public IcesClientVo queryById(Long clId){
+        return baseMapper.selectVoById(clId);
+    }
+
+    /**
+     * 查询客户基本信息列表
+     */
+    @Override
+    public TableDataInfo<IcesClientVo> queryPageList(IcesClientBo bo, PageQuery pageQuery) {
+        LambdaQueryWrapper<IcesClient> lqw = buildQueryWrapper(bo);
+        Page<IcesClientVo> result = baseMapper.selectVoPage(pageQuery.build(), lqw);
+        return TableDataInfo.build(result);
+    }
+
+    /**
+     * 查询客户基本信息列表
+     */
+    @Override
+    public List<IcesClientVo> queryList(IcesClientBo bo) {
+        LambdaQueryWrapper<IcesClient> lqw = buildQueryWrapper(bo);
+        return baseMapper.selectVoList(lqw);
+    }
+
+    private LambdaQueryWrapper<IcesClient> buildQueryWrapper(IcesClientBo bo) {
+        Map<String, Object> params = bo.getParams();
+        LambdaQueryWrapper<IcesClient> lqw = Wrappers.lambdaQuery();
+        lqw.eq(StringUtils.isNotBlank(bo.getCllCode()), IcesClient::getCllCode, bo.getCllCode());
+        lqw.eq(bo.getClOperator() != null, IcesClient::getClOperator, bo.getClOperator());
+        lqw.like(StringUtils.isNotBlank(bo.getClName()), IcesClient::getClName, bo.getClName());
+        lqw.eq(StringUtils.isNotBlank(bo.getClStat()), IcesClient::getClStat, bo.getClStat());
+        lqw.eq(bo.getClDelete() != null, IcesClient::getClDelete, bo.getClDelete());
+        return lqw;
+    }
+
+    /**
+     * 新增客户基本信息
+     */
+    @Override
+    public Boolean insertByBo(IcesClientBo bo) {
+        IcesClient add = BeanUtil.toBean(bo, IcesClient.class);
+        validEntityBeforeSave(add);
+        boolean flag = baseMapper.insert(add) > 0;
+        if (flag) {
+            bo.setClId(add.getClId());
+        }
+        return flag;
+    }
+
+    /**
+     * 修改客户基本信息
+     */
+    @Override
+    public Boolean updateByBo(IcesClientBo bo) {
+        IcesClient update = BeanUtil.toBean(bo, IcesClient.class);
+        validEntityBeforeSave(update);
+        return baseMapper.updateById(update) > 0;
+    }
+
+    /**
+     * 保存前的数据校验
+     */
+    private void validEntityBeforeSave(IcesClient entity){
+        //TODO 做一些数据校验,如唯一约束
+    }
+
+    /**
+     * 批量删除客户基本信息
+     */
+    @Override
+    public Boolean deleteWithValidByIds(Collection<Long> ids, Boolean isValid) {
+        if(isValid){
+            //TODO 做一些业务上的校验,判断是否需要校验
+        }
+        return baseMapper.deleteBatchIds(ids) > 0;
+    }
+}

180294866ec2349a57e0a8b64110c201b6e2fe6e
YangZY <2021212038@stu.hit.edu.cn>
2025-04-09 16:07:28 +0800

    引入编码

--- backend/ruoyi-system/src/main/java/com/ruoyi/system/mapper/IcesExceptionMessageRecordMapper.java
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/mapper/IcesExceptionMessageRecordMapper.java
@@ -3,6 +3,7 @@ package com.ruoyi.system.mapper;
 import com.ruoyi.system.domain.IcesExceptionMessageRecord;
 import com.ruoyi.system.domain.vo.IcesExceptionMessageRecordVo;
 import com.ruoyi.common.core.mapper.BaseMapperPlus;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 异常消息记录Mapper接口
@@ -10,6 +11,7 @@ import com.ruoyi.common.core.mapper.BaseMapperPlus;
  * @author ruoyi
  * @date 2025-04-09
  */
+@Mapper
 public interface IcesExceptionMessageRecordMapper extends BaseMapperPlus<IcesExceptionMessageRecordMapper, IcesExceptionMessageRecord, IcesExceptionMessageRecordVo> {
 
 }
--- backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesExceptionMessageRecordServiceImpl.java
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesExceptionMessageRecordServiceImpl.java
@@ -7,6 +7,7 @@ import com.ruoyi.common.core.domain.PageQuery;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ruoyi.system.service.IIcesCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.domain.bo.IcesExceptionMessageRecordBo;
@@ -30,6 +31,7 @@ import java.util.Collection;
 public class IcesExceptionMessageRecordServiceImpl implements IIcesExceptionMessageRecordService {
 
     private final IcesExceptionMessageRecordMapper baseMapper;
+    private final IIcesCodeService codeService;
 
     /**
      * 查询异常消息记录
@@ -74,6 +76,7 @@ public class IcesExceptionMessageRecordServiceImpl implements IIcesExceptionMess
      */
     @Override
     public Boolean insertByBo(IcesExceptionMessageRecordBo bo) {
+        bo.setExmrCode(codeService.insertByType("ExceptionMessageRecord"));
         IcesExceptionMessageRecord add = BeanUtil.toBean(bo, IcesExceptionMessageRecord.class);
         validEntityBeforeSave(add);
         boolean flag = baseMapper.insert(add) > 0;

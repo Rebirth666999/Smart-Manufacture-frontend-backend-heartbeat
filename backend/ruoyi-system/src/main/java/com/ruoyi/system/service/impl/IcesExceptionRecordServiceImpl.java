82e5aeae95cb933fe9e41c13ff7c7609258c715e
YangZY <2021212038@stu.hit.edu.cn>
2025-04-09 09:34:55 +0800

    引入编码

--- backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesExceptionRecordServiceImpl.java
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/IcesExceptionRecordServiceImpl.java
@@ -7,6 +7,7 @@ import com.ruoyi.common.core.domain.PageQuery;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ruoyi.system.service.IIcesCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.domain.bo.IcesExceptionRecordBo;
@@ -30,6 +31,7 @@ import java.util.Collection;
 public class IcesExceptionRecordServiceImpl implements IIcesExceptionRecordService {
 
     private final IcesExceptionRecordMapper baseMapper;
+    private final IIcesCodeService codeService;
 
     /**
      * 查询异常记录
@@ -75,6 +77,7 @@ public class IcesExceptionRecordServiceImpl implements IIcesExceptionRecordServi
      */
     @Override
     public Boolean insertByBo(IcesExceptionRecordBo bo) {
+        bo.setExrCode(codeService.insertByType("ExceptionRecord"));
         IcesExceptionRecord add = BeanUtil.toBean(bo, IcesExceptionRecord.class);
         validEntityBeforeSave(add);
         boolean flag = baseMapper.insert(add) > 0;

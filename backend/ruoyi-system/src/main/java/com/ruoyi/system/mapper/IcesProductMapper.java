b829aa0143dfc284f49b1e3d7e653381b08b5cc6
YangZY <2021212038@stu.hit.edu.cn>
2025-04-17 14:47:23 +0800

    代码生成-产品管理

---
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/mapper/IcesProductMapper.java
@@ -0,0 +1,15 @@
+package com.ruoyi.system.mapper;
+
+import com.ruoyi.system.domain.IcesProduct;
+import com.ruoyi.system.domain.vo.IcesProductVo;
+import com.ruoyi.common.core.mapper.BaseMapperPlus;
+
+/**
+ * 产品Mapper接口
+ *
+ * @author ruoyi
+ * @date 2025-04-17
+ */
+public interface IcesProductMapper extends BaseMapperPlus<IcesProductMapper, IcesProduct, IcesProductVo> {
+
+}

f33161c978be48bc99626707c4b35704adab0c82
YangZY <2021212038@stu.hit.edu.cn>
2025-04-17 14:48:52 +0800

    引入编码

--- backend/ruoyi-system/src/main/java/com/ruoyi/system/mapper/IcesProductMapper.java
+++ backend/ruoyi-system/src/main/java/com/ruoyi/system/mapper/IcesProductMapper.java
@@ -3,6 +3,7 @@ package com.ruoyi.system.mapper;
 import com.ruoyi.system.domain.IcesProduct;
 import com.ruoyi.system.domain.vo.IcesProductVo;
 import com.ruoyi.common.core.mapper.BaseMapperPlus;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 产品Mapper接口
@@ -10,6 +11,7 @@ import com.ruoyi.common.core.mapper.BaseMapperPlus;
  * @author ruoyi
  * @date 2025-04-17
  */
+@Mapper
 public interface IcesProductMapper extends BaseMapperPlus<IcesProductMapper, IcesProduct, IcesProductVo> {
 
 }

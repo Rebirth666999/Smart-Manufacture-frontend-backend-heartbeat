285fa0f0a00a30cda68a7be3b0bf59d7637a00f5
YangZY <2021212038@stu.hit.edu.cn>
2025-04-21 15:00:35 +0800

    代码格式化

--- frontend/src/views/system/productInspection/index.vue
+++ frontend/src/views/system/productInspection/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="app-container">
+  <div class="app-container">
     <el-card class="view-card">
       <div slot="header">
         <div class="card-header">
@@ -7,166 +7,93 @@
         </div>
       </div>
       <div>
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="质检单编码" prop="piCode">
-        <el-input
-          v-model="queryParams.piCode"
-          placeholder="请输入质检单编码"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="批次" prop="pbCode">
-          <el-select
-          v-model="queryParams.pbCode"
-          placeholder="请选择批次"
-        >
-          <el-option
-            v-for="item in productBatchList"
-            :key="item.pbCode"
-            :label="item.pbBatch"
-            :value="item.pbCode"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <!-- <el-form-item label="已删除" prop="piDelete">
-        <el-input
-          v-model="queryParams.piDelete"
-          placeholder="请输入已删除"
-          clearable
-          @keyup.enter.native="handleQuery"
-        /> -->
-      <!-- </el-form-item> -->
-      <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
-      </el-form-item>
-    </el-form>
+        <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch"
+          label-width="68px">
+          <el-form-item label="批次" prop="pbCode">
+            <el-select v-model="queryParams.pbCode" placeholder="请选择批次">
+              <el-option v-for="item in productBatchList" :key="item.pbCode" :label="item.pbBatch" :value="item.pbCode">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <!-- <el-form-item label="已删除" prop="piDelete">
+            <el-input v-model="queryParams.piDelete" placeholder="请输入已删除" clearable @keyup.enter.native="handleQuery" />
+          </el-form-item> -->
+          <el-form-item>
+            <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+            <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+          </el-form-item>
+        </el-form>
 
-    <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['system:productInspection:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:productInspection:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['system:productInspection:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['system:productInspection:export']"
-        >导出</el-button>
-      </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
-    </el-row>
+        <el-row :gutter="10" class="mb8">
+          <el-col :span="1.5">
+            <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
+              v-hasPermi="['system:productInspection:add']">新增</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button type="success" plain icon="el-icon-edit" size="mini" :disabled="single" @click="handleUpdate"
+              v-hasPermi="['system:productInspection:edit']">修改</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple" @click="handleDelete"
+              v-hasPermi="['system:productInspection:remove']">删除</el-button>
+          </el-col>
+          <el-col :span="1.5">
+            <el-button type="warning" plain icon="el-icon-download" size="mini" @click="handleExport"
+              v-hasPermi="['system:productInspection:export']">导出</el-button>
+          </el-col>
+          <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+        </el-row>
 
-    <el-table
-          v-loading="loading"
-          :data="productInspectionList"
-          @current-change="handleCurrentChange"
-          highlight-current-row
-          max-height="240"
-        >
+        <el-table v-loading="loading" :data="productInspectionList" @current-change="handleCurrentChange"
+          highlight-current-row max-height="240">
           <el-table-column label="选择" width="55" align="center">
             <template slot-scope="scope">
               <el-radio :value="scope.row.orId === idSelect" :label="true" />
             </template>
           </el-table-column>
-      <el-table-column label="质检单ID" align="center" prop="piId" v-if="true"/>
-      <el-table-column label="质检单编码" align="center" prop="piCode" />
-      <el-table-column label="批次" align="center" prop="pbCode" />
-      <el-table-column label="负责人" align="center" prop="piMan" />
-      <el-table-column label="开始日期" align="center" prop="piSdate" />
-      <el-table-column label="部门" align="center" prop="piDept" />
-      <el-table-column label="结束日期" align="center" prop="piEdate" />
-      <!-- <el-table-column label="已删除" align="center" prop="piDelete" /> -->
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['system:productInspection:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['system:productInspection:remove']"
-          >删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+          <el-table-column label="质检单ID" align="center" prop="piId" v-if="true" />
+          <el-table-column label="质检单编码" align="center" prop="piCode" />
+          <el-table-column label="批次" align="center" prop="pbCode" />
+          <el-table-column label="负责人" align="center" prop="piMan" />
+          <el-table-column label="开始日期" align="center" prop="piSdate" />
+          <el-table-column label="部门" align="center" prop="piDept" />
+          <el-table-column label="结束日期" align="center" prop="piEdate" />
+          <!-- <el-table-column label="已删除" align="center" prop="piDelete" /> -->
+          <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+            <template slot-scope="scope">
+              <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
+                v-hasPermi="['system:productInspection:edit']">修改</el-button>
+              <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
+                v-hasPermi="['system:productInspection:remove']">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
 
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
-    />
+        <pagination v-show="total > 0" :total="total" :page.sync="queryParams.pageNum"
+          :limit.sync="queryParams.pageSize" @pagination="getList" />
 
-    <!-- 添加或修改产品质检单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="批次" prop="pbCode">
-          <el-select
-          v-model="form.pbCode"
-          placeholder="请选择批次"
-        >
-          <el-option
-            v-for="item in productBatchList"
-            :key="item.pbCode"
-            :label="item.pbBatch"
-            :value="item.pbCode"
-          >
-          </el-option>
-        </el-select>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
+        <!-- 添加或修改产品质检单对话框 -->
+        <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+          <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+            <el-form-item label="批次" prop="pbCode">
+              <el-select v-model="form.pbCode" placeholder="请选择批次">
+                <el-option v-for="item in productBatchList" :key="item.pbCode" :label="item.pbBatch"
+                  :value="item.pbCode">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+            <el-button @click="cancel">取 消</el-button>
+          </div>
+        </el-dialog>
       </div>
-    </el-dialog>
-      </div>
-  </el-card>
+    </el-card>
     <el-card class="controlled-card">
       <div slot="header">
         <div class="card-header">
-          <div>产品质检单信息</div>
+          <div>产品质检单明细</div>
         </div>
       </div>
       <order-demand v-if='idSelect' :key="idSelect" :orCode="codeSelect" />
@@ -365,22 +292,27 @@ export default {
 .el-select {
   width: 100%;
 }
-.el-date-editor{
+
+.el-date-editor {
   width: 100%;
 }
+
 ::v-deep .el-radio span.el-radio__label {
   display: none;
 }
+
 .view-card {
   max-height: 50vh;
   overflow: scroll;
 }
+
 .card-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-size: 17px;
 }
+
 .controlled-card {
   margin-top: 10px;
 }

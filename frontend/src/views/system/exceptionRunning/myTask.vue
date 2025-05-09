839077f68c5f219796a1630161875d3a7c0c626b
YangZY <2021212038@stu.hit.edu.cn>
2025-05-09 16:28:16 +0800

    流程图片裁剪

--- frontend/src/views/system/exceptionRunning/myTask.vue
+++ frontend/src/views/system/exceptionRunning/myTask.vue
@@ -72,7 +72,7 @@
         </el-form>
         <!-- <process-viewer :key="`designer-${processView.index}`" :xml="processView.xmlData" :style="{'height': '300px', 'margin-bottom': '2em'}" /> -->
         <div>
-          <img v-if="processView.img" :src="processView.img" />
+          <el-image v-if="processView.img" :src="processView.img" />
         </div>
         <div slot="footer" class="dialog-footer">
           <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 认</el-button>
@@ -181,9 +181,70 @@ export default {
       this.currentTask = row
       this.processView.index = row.procDefId
       this.open = true
+      const that = this
       getProcessFlowXml(row.procInsId).then(response => {
         const blob = new Blob([response], { type: 'image/png' })
-        this.processView.img = window.URL.createObjectURL(blob)
+        const url = window.URL.createObjectURL(blob)
+        // 渲染图片得到图片信息
+        const canvas = document.createElement("canvas")
+        const ctx = canvas.getContext("2d")
+        const image = new Image()
+        image.src = url
+        image.onload = function () {
+          canvas.width = image.width
+          canvas.height = image.height
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+          const { data, width, height } = imageData;
+
+          let startX = width
+          let startY = height
+          let endX = 0
+          let endY = 0
+          // 像素遍历
+          for (let col = 0; col < width; col++) {
+            for (let row = 0; row < height; row++) {
+              const pxStartIndex = (row * width + col) * 4;
+              const pxData = {
+                r: data[pxStartIndex],
+                g: data[pxStartIndex + 1],
+                b: data[pxStartIndex + 2],
+                a: data[pxStartIndex + 3]
+              };
+              // 不透明就是有颜色
+              const colorExist = pxData.a !== 0;
+              if (colorExist) {
+                startX = Math.min(col, startX);
+                endX = Math.max(col, startX);
+                startY = Math.min(row, startY);
+                endY = Math.max(row, endY);
+              }
+            }
+          }
+          // 追加边距
+          endX += 11;
+          endY += 11;
+          startX -= 10;
+          startY -= 10;
+          // 渲染新的图片
+          const cropCanvas = document.createElement("canvas");
+          const cropCtx = cropCanvas.getContext("2d");
+          cropCanvas.width = endX - startX;
+          cropCanvas.height = endY - startY;
+          cropCtx.drawImage(
+            image,
+            startX,
+            startY,
+            cropCanvas.width,
+            cropCanvas.height,
+            0,
+            0,
+            cropCanvas.width,
+            cropCanvas.height
+          );
+          // 渲染结束返回url
+          that.processView.img = cropCanvas.toDataURL()
+        }
       })
       getProcessXml(row.procDefId).then(response => {
         this.processView.xmlData = response.data

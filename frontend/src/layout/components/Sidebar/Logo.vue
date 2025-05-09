fdb3c37a0505a607e98d2bf2b7b8112ef4a4a82a
YangZY <2021212038@stu.hit.edu.cn>
2025-05-09 22:52:57 +0800

    静态文件改进

--- frontend/src/layout/components/Sidebar/Logo.vue
+++ frontend/src/layout/components/Sidebar/Logo.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import logoImg from '@/assets/logo/image.png'
+import logoImg from '@/assets/logo/image_with_bg.png'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
